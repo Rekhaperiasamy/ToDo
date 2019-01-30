eda9b8d8cc04362ae34e6d5917b81ec664cdca58
Rekha <rekha_p@outlook.com>
2019-01-30 23:53:48 +0530

    Project Setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Welcome to ToDo Application.
+            Welcome to ToDo Application...
           </p>
         </header>
       </div>
